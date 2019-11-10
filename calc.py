d2e3c240b0246d611a678c9db4de9688dc7ed746
SURYA PRAKASH GIRI <57126240+suryaprakashgiri01@users.noreply.github.com>
2019-11-10 20:09:15 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,43 @@
+
+# This is a smart calculator
+
+print()
+print('     ------------------------------------------------------------------')
+print('     |                  Welcome to the mini calculator                |')
+print('     ------------------------------------------------------------------')
+print()
+con = True
+while(con):
+	print()
+	print('Enter the exptession as num1 sign num2 :',end=' ')
+	exp = input()
+	exp_l =  exp.split(' ')
+	print()
+
+	if exp_l[1] == '+':
+		result = int(exp_l[0]) + int(exp_l[2])
+		print(exp+' = '+str(result))
+	elif exp_l[1] == '-':
+        	result =  int(exp_l[0]) - int(exp_l[2])
+        	print(exp+' = '+str(result))
+	elif exp_l[1] == '*' or exp_l[1] =='×':
+        	result = int(exp_l[0]) * int(exp_l[2])
+        	print(exp+' = '+str(result))
+	elif exp_l[1] == '/' or exp_l[1] == '÷':
+        	result = int(exp_l[0]) / int(exp_l[2])
+        	print(exp+' = '+str(result))
+	elif exp_l[1] == '%':
+        	result = int(exp_l[0]) % int(exp_l[2])
+        	print(exp+' = '+str(result))
+	elif exp_l[1] == '^':
+        	result = int(exp_l[0]) ** int(exp_l[2])
+        	print(exp+' = '+str(result))
+	else:
+		print('Invalid Type of Input')
+
+	print('\nDo you want to continue[y/n] :',end=' ')
+	x = input()
+	if x=='n' or x=='N':
+		print('\nThank you for using...')
+		con = False
+print()
